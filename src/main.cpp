91f3de91d549664a49b5b0073d10cd4b68374271
hupeyaszih <hupeya@protonmail.com>
2026-01-18 16:24:36 +0300

    Init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "core/simulation.h"
+#include "renderer/renderer.h"
 int main() {
+    //Init Simulation
     bool simulationRunning = true;
     Simulation simulation;
     simulation.Init();
+
+    //Init Renderer
+    Renderer renderer;
+    renderer.Init();
+
     /*
     while (simulationRunning) {
         simulation.Update();
--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,13 +1,14 @@
 #include "renderer/renderer.h"
+#include "globals.h"
 
 Renderer::Renderer(){
 
 }
 
 void Renderer::Init() {
-
+    LOG("Renderer Initialized");
 }
 
 void Renderer::Render() {
-    
+
 }
